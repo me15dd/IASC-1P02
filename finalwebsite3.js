ad5279228a15a48c740833f2c3a338ed50121220
me15dd <31972329+me15dd@users.noreply.github.com>
2017-12-06 23:48:25 -0500

    finalwebsite3.js

---
+++ finalwebsite3.js
@@ -0,0 +1,21 @@
+//top ups
+var age = prompt("What is your age?");
+var date = new Date();
+var year = date.getFullYear();
+var birthYear = year - age;
+
+//time
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
